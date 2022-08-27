c570542aa9e5017a4b69a16f8466a71ae1e7aa86
unknown <Cedran-Bastien>
2022-08-27 19:56:44 +0200

    Debut frise

--- Jeu/Frise.go
+++ Jeu/Frise.go
@@ -1,1 +1,14 @@
 package Jeu
+
+import "Timeline_project/gestionPaquetCarte"
+
+type Frise struct {
+	frise []gestionPaquetCarte.Carte
+}
+
+func creerFrise(paquet gestionPaquetCarte.Paquet) (frise Frise) {
+	var c gestionPaquetCarte.Carte = gestionPaquetCarte.PiocherCarte(paquet)
+
+}
+
+
